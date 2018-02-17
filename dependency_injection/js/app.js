036b391290c9b3648f9967f4c24c2bd2227e9293
padoura <padoura@users.noreply.github.com>
2018-02-17 23:22:42 +0200

    minified javascript suitably for AngularJS

--- dependency_injection/js/app.js
+++ dependency_injection/js/app.js
@@ -1,22 +1,25 @@
-(function () {
-'use strict';
+!function(){"use strict";function e(n,o,r){n.name="",n.upper=function(){var e=o("uppercase");n.upperName=e(n.name)},console.log(r.annotate(e))}angular.module("DIApp",[]).controller("DIController",e),e.$inject=["$scope","$filter","$injector"]}();
 
-angular.module('DIApp', [])
-.controller(
-  'DIController', DIController);
-
-
-  function DIController ($scope, $filter, $injector) {
-    $scope.name = "";
-
-    $scope.upper = function () {
-      var upCase = $filter('uppercase');
-      $scope.upperName = upCase($scope.name);
-    };
-
-    console.log($injector.annotate(DIController));
-
-  };
-
-
-})();
+// (function () {
+// 'use strict';
+//
+// angular.module('DIApp', [])
+// .controller(
+//   'DIController', DIController);
+//
+//   DIController.$inject = ['$scope', "$filter", "$injector"];
+//
+//   function DIController ($scope, $filter, $injector) {
+//     $scope.name = "";
+//
+//     $scope.upper = function () {
+//       var upCase = $filter('uppercase');
+//       $scope.upperName = upCase($scope.name);
+//     };
+//
+//     console.log($injector.annotate(DIController));
+//
+//   };
+//
+//
+// })();
